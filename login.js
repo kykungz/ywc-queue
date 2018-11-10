1dad8a7fe4814cfbc184319823db28e6ed21465e
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-11-10 23:51:23 +0700

    :sparkles: Add signout feature

--- login.js
+++ login.js
@@ -5,14 +5,21 @@ var mc = new Hammer.Manager(logo)
 mc.add(new Hammer.Press({ time: 2000 }))
 
 mc.on('press', function(ev) {
-  const password = prompt('Enter password')
-  firebase
-    .auth()
-    .signInWithEmailAndPassword('jackykongpon@gmail.com', password)
-    .then(() => {
-      alert('Welcome Admin!')
-    })
-    .catch(() => {
-      alert('Incorrect Password')
-    })
+  if (firebase.auth().currentUser) {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => alert('Logged out'))
+  } else {
+    const password = prompt('Enter password')
+    firebase
+      .auth()
+      .signInWithEmailAndPassword('jackykongpon@gmail.com', password)
+      .then(() => {
+        alert('Welcome Admin!')
+      })
+      .catch(() => {
+        alert('Incorrect Password')
+      })
+  }
 })
