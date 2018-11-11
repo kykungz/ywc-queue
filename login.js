e287c2932d107a34ea0418d791fd4474a2966d70
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-11-11 08:46:23 +0700

    :sparkles: Change login method

--- login.js
+++ login.js
@@ -1,10 +1,36 @@
-var logo = document.getElementById('ywc-logo')
+const logo = document.getElementById('ywc-logo')
 
-var mc = new Hammer.Manager(logo)
+const mc = new Hammer.Manager(logo)
 
 mc.add(new Hammer.Press({ time: 2000 }))
 
 mc.on('press', function(ev) {
+  if (firebase.auth().currentUser) {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => alert('Logged out'))
+  } else {
+    const password = prompt('Enter password')
+    firebase
+      .auth()
+      .signInWithEmailAndPassword('jackykongpon@gmail.com', password)
+      .then(() => {
+        alert('Welcome Admin!')
+      })
+      .catch(() => {
+        alert('Incorrect Password')
+      })
+  }
+})
+
+const text = document.getElementById('check-queue-text')
+
+const mc2 = new Hammer.Manager(text)
+
+mc2.add(new Hammer.Press({ time: 2000 }))
+
+mc2.on('press', function(ev) {
   if (firebase.auth().currentUser) {
     firebase
       .auth()
